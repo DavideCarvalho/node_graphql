4a62935d906e0d412da27c1a40ff5b17e74c248a
Davi de Carvalho <davicarvalho96@yahoo.com.br>
2017-11-27 17:23:08 -0200

    adding old project files so I can merge them later with newer ones

---
+++ old_start/front_old/src/high_order_components/login.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+  render () {
+    return (
+      <div>
+          <h1>Login Component is rendered</h1>
+      </div>
+    )
+  }
+}
